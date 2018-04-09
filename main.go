13b5f15e30eeca641c74d04eff558a096331cf74
Hernan Paez <lhernan.paez@gmail.com>
2018-04-09 19:07:42 -0300

    Changed Licensing

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2018 Luis go Hernan Paez
+Copyright (c) 2018 Luis Hernan Paez
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
