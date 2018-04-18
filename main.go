fdc3bd3ed8ebf5c6fc8c7a356e00a01e7c87e442
Hernan Paez <lhernan.paez@gmail.com>
2018-04-18 15:49:27 -0300

    Test

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ SOFTWARE.
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"image"
@@ -36,6 +37,22 @@ import (
 
 	"github.com/nfnt/resize"
 )
+
+type AssetImage struct {
+	Idiom    string `json:"idiom"`
+	Filename string `json:"filename"`
+	Scale    string `json:"scale"`
+}
+
+type AssetInfo struct {
+	Version int    `json:"version"`
+	Author  string `json:"author"`
+}
+
+type AssetCatalog struct {
+	Images []AssetImage `json:"images"`
+	Info   AssetInfo    `json:"author"`
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -60,25 +77,7 @@ func main() {
 	}
 
 	if stat.IsDir() {
-		files, err := ioutil.ReadDir(path)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Directory Reading Error: %s", err.Error())
-			os.Exit(1)
-			return
-		}
-
-		for _, f := range files {
-			if f.IsDir() == false && strings.ToLower(filepath.Ext(f.Name())) == ".png" {
-
-				err := resizeFileAtPath(path + "/" + f.Name())
-				if err != nil {
-					os.Exit(1)
-					return
-				}
-
-			}
-		}
-
+		scanDir(path, true, true)
 	} else {
 		resizeFileAtPath(path)
 	}
@@ -87,107 +86,197 @@ func main() {
 	os.Exit(0)
 }
 
-//Scales an image from @2x -> @3x @1x
-func scaleFromRetina(img1 image.Image, name string) error {
-	originalWidth := float64(img1.Bounds().Max.X)
-	originalHeight := float64(img1.Bounds().Max.Y)
+func getCleanName(filename string) string {
+	name := strings.TrimSuffix(filename, filepath.Ext(filename))
+	atIdx := strings.Index(name, "@")
+	if atIdx != -1 {
+		name = name[0:atIdx]
+	}
+	return name
+}
 
-	fmt.Fprintf(os.Stdout, "\tCreating @3x file\n")
-	err := scaleImage(img1, uint(originalWidth*1.5), uint(originalHeight*1.5), name, "@3x")
+func getFileNameFromPath(path string) string {
+	idx := strings.LastIndex(path, "/")
+	if idx != -1 && len(path) > idx+1 {
+		return path[idx+1 : len(path)]
+	}
+	return path
+}
+
+func getDirFromPath(path string) string {
+	idx := strings.LastIndex(path, "/")
+	if idx != -1 {
+		return path[0:idx]
+	}
+	return path
+}
+
+func scanDir(path string, recursive bool, createAssetCatalog bool) error {
+	files, err := ioutil.ReadDir(path)
 	if err != nil {
-		return errors.New("Resize Error: " + err.Error())
+		return err
 	}
 
-	fmt.Fprintf(os.Stdout, "\tRenaming @2x file\n")
-	err = scaleImage(img1, uint(originalWidth), uint(originalHeight), name, "@2x")
-	if err != nil {
-		return errors.New("Resize Error: %s" + err.Error())
-	}
+	for _, f := range files {
+		fpath := path + "/" + f.Name()
+		if f.IsDir() == false && strings.ToLower(filepath.Ext(f.Name())) == ".png" {
+			//Get a clean filename, remove extension and any scale reference ie: @2x @3x
 
-	fmt.Fprintf(os.Stdout, "\tCreating @1x file\n")
-	err = scaleImage(img1, uint(originalWidth*0.5), uint(originalHeight*0.5), name, "")
-	if err != nil {
-		return errors.New("Resize Error: %s" + err.Error())
+			//Generate scaled files
+			assets, err := resizeFileAtPath(fpath)
+			if err != nil {
+				return err
+			}
+
+			//create json file
+			if createAssetCatalog {
+				info := AssetInfo{Version: 1, Author: "asset-resizer"}
+				catalog := AssetCatalog{Images: assets, Info: info}
+				saveJSON(catalog, getDirFromPath(fpath))
+			}
+
+		} else if f.IsDir() && recursive {
+			if err := scanDir(fpath, recursive, createAssetCatalog); err != nil {
+				return err
+			}
+		}
 	}
 
 	return nil
 }
 
-func resizeFileAtPath(path string) error {
-	name := strings.TrimSuffix(path, filepath.Ext(path))
+//Scales an image from @2x -> @3x @1x
+func scaleFromRetina(img1 image.Image, path string) ([]AssetImage, error) {
+	destinationPath := getCleanName(path)
+
+	originalWidth := float64(img1.Bounds().Max.X)
+	originalHeight := float64(img1.Bounds().Max.Y)
+
+	assets := make([]AssetImage, 3)
+
+	fmt.Fprintf(os.Stdout, "\tCreating @3x file\n")
+	path, err := scaleImage(img1, uint(originalWidth*1.5), uint(originalHeight*1.5), destinationPath, "@3x")
+	if err != nil {
+		return nil, errors.New("Resize Error: " + err.Error())
+	}
+	assets[0] = AssetImage{Idiom: "universal", Filename: getFileNameFromPath(path), Scale: "3x"}
+
+	fmt.Fprintf(os.Stdout, "\tRenaming @2x file\n")
+	path, err = scaleImage(img1, uint(originalWidth), uint(originalHeight), destinationPath, "@2x")
+	if err != nil {
+		return nil, errors.New("Resize Error: %s" + err.Error())
+	}
+	assets[1] = AssetImage{Idiom: "universal", Filename: getFileNameFromPath(path), Scale: "2x"}
+
+	fmt.Fprintf(os.Stdout, "\tCreating @1x file\n")
+	path, err = scaleImage(img1, uint(originalWidth*0.5), uint(originalHeight*0.5), destinationPath, "")
+	if err != nil {
+		return nil, errors.New("Resize Error: %s" + err.Error())
+	}
+	assets[2] = AssetImage{Idiom: "universal", Filename: getFileNameFromPath(path), Scale: "1x"}
+
+	return nil, nil
+}
+
+func resizeFileAtPath(path string) ([]AssetImage, error) {
 	fmt.Fprintf(os.Stdout, "\n- Resizing file: %s", path)
 
 	fImg, err := os.Open(path)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Open Error: %s", err.Error())
-		return err
+		return nil, err
 	}
 	defer fImg.Close()
 
 	img1, _, err := image.Decode(fImg)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Decode Error: %s", err.Error())
-		return err
+		return nil, err
 	}
 
-	if strings.Contains(name, "@2x") {
+	if strings.Contains(strings.ToLower(path), "@2x") {
 		fmt.Fprintf(os.Stdout, "\n- Resizing from @2x...\n")
-		name = strings.TrimSuffix(name, "@2x")
-		err = scaleFromRetina(img1, name)
+		assets, err := scaleFromRetina(img1, path)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%s", err.Error())
-			return err
+			return nil, err
 		}
+
+		return assets, nil
+
 	} else {
 		fmt.Fprintf(os.Stdout, "\n- Resizing from @3x...\n")
-		name = strings.TrimSuffix(name, "@3x")
-		err = scaleFromSuperRetina(img1, name)
+		assets, err := scaleFromSuperRetina(img1, path)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%s", err.Error())
-			return err
+			return nil, err
 		}
+
+		return assets, nil
 	}
-
-	return nil
-
 }
 
 //Scales an image from @3x -> @2x @1x
-func scaleFromSuperRetina(img1 image.Image, name string) error {
+func scaleFromSuperRetina(img1 image.Image, path string) ([]AssetImage, error) {
+	destinationPath := getCleanName(path)
 	originalWidth := float64(img1.Bounds().Max.X)
 	originalHeight := float64(img1.Bounds().Max.Y)
 
+	assets := make([]AssetImage, 3)
+
 	fmt.Fprintf(os.Stdout, "\tRenaming @3x file\n")
-	err := scaleImage(img1, uint(originalWidth), uint(originalHeight), name, "@3x")
+	path, err := scaleImage(img1, uint(originalWidth), uint(originalHeight), destinationPath, "@3x")
 	if err != nil {
-		return errors.New("Resize Error: " + err.Error())
+		return nil, errors.New("Resize Error: " + err.Error())
 	}
+	assets[0] = AssetImage{Idiom: "universal", Filename: getFileNameFromPath(path), Scale: "3x"}
 
 	fmt.Fprintf(os.Stdout, "\tCreating @2x file\n")
-	err = scaleImage(img1, uint(originalWidth*0.66), uint(originalHeight*0.66), name, "@2x")
+	path, err = scaleImage(img1, uint(originalWidth*0.66), uint(originalHeight*0.66), destinationPath, "@2x")
 	if err != nil {
-		return errors.New("Resize Error: %s" + err.Error())
+		return nil, errors.New("Resize Error: %s" + err.Error())
 	}
+	assets[1] = AssetImage{Idiom: "universal", Filename: getFileNameFromPath(path), Scale: "2x"}
 
 	fmt.Fprintf(os.Stdout, "\tCreating @1x file\n")
-	err = scaleImage(img1, uint(originalWidth*0.33), uint(originalHeight*0.33), name, "")
+	path, err = scaleImage(img1, uint(originalWidth*0.33), uint(originalHeight*0.33), destinationPath, "")
 	if err != nil {
-		return errors.New("Resize Error: %s" + err.Error())
+		return nil, errors.New("Resize Error: %s" + err.Error())
 	}
+	assets[2] = AssetImage{Idiom: "universal", Filename: getFileNameFromPath(path), Scale: "1x"}
 
-	return nil
+	return assets, nil
 }
 
-func scaleImage(img image.Image, width uint, height uint, filename string, suffix string) error {
+func saveJSON(catalog AssetCatalog, path string) error {
+	raw, err := json.Marshal(catalog)
+	if err != nil {
+		return err
+	}
 
-	newImage := resize.Resize(width, height, img, resize.Lanczos3)
-
-	file, err := os.Create(filename + suffix + ".png")
+	jsonPath := path + "/Contents.json"
+	file, err := os.Create(jsonPath)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
+	file.Write(raw)
+
+	return nil
+}
+
+func scaleImage(img image.Image, width uint, height uint, path string, suffix string) (string, error) {
+
+	newImage := resize.Resize(width, height, img, resize.Lanczos3)
+
+	newPath := path + suffix + ".png"
+	file, err := os.Create(newPath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
 	err = png.Encode(file, newImage)
-	return err
+	return newPath, err
 }
